0cdc530d9d86df8dca32f97a954c35896182301a
yashrb24 <bhisikaryash2403@gmail.com>
2023-12-17 18:55:24 +0530

    part 1 completed

---
+++ 2023/Day6/main.py
@@ -0,0 +1,34 @@
+def part1(data):
+    import math 
+
+    T = list(map(int, data[0].split()[1:]))
+    X = list(map(int, data[1].split()[1:]))
+
+    prod = 1
+    for t, x in zip(T, X):
+        left = (t - (t**2 - 4*x)**0.5) / 2
+        if int(left) == left:
+            left += 1
+        else:
+            left = math.ceil(left)
+
+        right = (t + (t**2 - 4*x)**0.5) / 2
+        if int(right) == right:
+            right -= 1
+        else:
+            right = math.floor(right)
+
+        prod *= (right - left + 1)
+
+
+    print(prod)
+
+def part2(data):
+    pass
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        data = f.read().splitlines()
+    part1(data)
+    part2(data)
