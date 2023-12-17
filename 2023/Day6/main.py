1f1afc37169d6435e8599e1661942f7ecd66df31
yashrb24 <bhisikaryash2403@gmail.com>
2023-12-17 18:58:41 +0530

    part 2 completed

--- 2023/Day6/main.py
+++ 2023/Day6/main.py
@@ -1,34 +1,50 @@
-def part1(data):
-    import math 
+import math
 
+
+def part1(data):
     T = list(map(int, data[0].split()[1:]))
     X = list(map(int, data[1].split()[1:]))
 
     prod = 1
     for t, x in zip(T, X):
-        left = (t - (t**2 - 4*x)**0.5) / 2
+        left = (t - (t**2 - 4 * x) ** 0.5) / 2
         if int(left) == left:
             left += 1
         else:
             left = math.ceil(left)
 
-        right = (t + (t**2 - 4*x)**0.5) / 2
+        right = (t + (t**2 - 4 * x) ** 0.5) / 2
         if int(right) == right:
             right -= 1
         else:
             right = math.floor(right)
 
-        prod *= (right - left + 1)
-
+        prod *= right - left + 1
 
     print(prod)
 
+
 def part2(data):
-    pass
+    t = int("".join(data[0].split()[1:]))
+    x = int("".join(data[1].split()[1:]))
+
+    left = (t - (t**2 - 4 * x) ** 0.5) / 2
+    if int(left) == left:
+        left += 1
+    else:
+        left = math.ceil(left)
+
+    right = (t + (t**2 - 4 * x) ** 0.5) / 2
+    if int(right) == right:
+        right -= 1
+    else:
+        right = math.floor(right)
+
+    print(right - left + 1)
 
 
-if __name__ == '__main__':
-    with open('input.txt') as f:
+if __name__ == "__main__":
+    with open("input.txt") as f:
         data = f.read().splitlines()
     part1(data)
     part2(data)
