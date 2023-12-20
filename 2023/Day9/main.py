a957e43459875dad67840dbcdea7d21ddc2a3c39
yashrb24 <bhisikaryash2403@gmail.com>
2023-12-20 13:11:34 +0530

    part 2 completed

--- 2023/Day9/main.py
+++ 2023/Day9/main.py
@@ -20,7 +20,25 @@ def part1(data):
 
 
 def part2(data):
-    pass
+    total = 0
+    for line in data:
+        nums = list(map(int, line.split()))
+        arrays = [nums[::-1]]
+        
+        while True:
+            new_array = [arrays[-1][i+1] - arrays[-1][i] for i in range(len(arrays[-1]) - 1)]
+            arrays.append(new_array)
+            if all([x == 0 for x in new_array]):
+                break
+
+        for i in range(len(arrays) - 1, 0, -1):
+            arrays[i-1].append(arrays[i][-1] + arrays[i-1][-1])
+
+        total += arrays[0][-1]
+        
+    print(total)
+
+        
 
 
 if __name__ == '__main__':
