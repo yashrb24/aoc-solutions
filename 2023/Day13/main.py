a8c2b8e238f585085ea20aac115a176732d075c9
yashrb24 <bhisikaryash2403@gmail.com>
2023-12-25 00:08:57 +0530

    progress pt2

--- 2023/Day13/main.py
+++ 2023/Day13/main.py
@@ -46,7 +46,121 @@ def part1(data):
 
 
 def part2(data):
-    pass
+    grid = []
+    total = 0
+
+    for line in data:
+        if line == "":
+            orig_h = find_horizontal_reflection(grid)
+            orig_v = find_vertical_reflection(grid)
+
+            n, m = len(grid), len(grid[0])
+            for x in range(n):
+                found = False
+
+                for y in range(m):
+                    curr = grid[x][y]
+
+                    if curr == "#":
+                        tmp = list(grid[x])
+                        tmp[y] = "."
+                        grid[x] = "".join(tmp)
+
+                        new_h = find_horizontal_reflection(grid)
+                        new_v = find_vertical_reflection(grid)
+
+                        tmp[y] = "#"
+                        grid[x] = "".join(tmp)
+
+                        if not (new_h == new_v == 0) and not (
+                            new_h == orig_h and new_v == orig_v
+                        ):
+                            found = True
+                            total += new_h * 100 + new_v
+                            print(f"Found at {x = } and {y = }")
+                            break
+
+                    else:
+                        tmp = list(grid[x])
+                        tmp[y] = "#"
+                        grid[x] = "".join(tmp)
+
+                        new_h = find_horizontal_reflection(grid)
+                        new_v = find_vertical_reflection(grid)
+
+                        tmp[y] = "."
+                        grid[x] = "".join(tmp)
+
+                        if not (new_h == new_v == 0) and not (
+                            new_h == orig_h and new_v == orig_v
+                        ):
+                            found = True
+                            total += new_h * 100 + new_v
+                            print(f"Found at {x = } and {y = }")
+                            break
+
+
+                if found:
+                    break
+
+            grid = []
+
+        else:
+            grid.append(line)
+
+
+    orig_h = find_horizontal_reflection(grid)
+    orig_v = find_vertical_reflection(grid)
+
+    n, m = len(grid), len(grid[0])
+    for x in range(n):
+        found = False
+
+        for y in range(m):
+            curr = grid[x][y]
+            
+            if curr == "#":
+                tmp = list(grid[x])
+                tmp[y] = "."
+                grid[x] = "".join(tmp)
+
+                new_h = find_horizontal_reflection(grid)
+                new_v = find_vertical_reflection(grid)
+
+                tmp[y] = "#"
+                grid[x] = "".join(tmp)
+
+                if not (new_h == new_v == 0) and not (
+                    new_h == orig_h and new_v == orig_v
+                ):
+                    found = True
+                    total += new_h * 100 + new_v
+                    print(f"Found at {x = } and {y = }")
+                    break
+
+            else:
+                tmp = list(grid[x])
+                tmp[y] = "#"
+                grid[x] = "".join(tmp)
+
+                new_h = find_horizontal_reflection(grid)
+                new_v = find_vertical_reflection(grid)
+
+                tmp[y] = "."
+                grid[x] = "".join(tmp)
+
+                if not (new_h == new_v == 0) and not (
+                    new_h == orig_h and new_v == orig_v
+                ):
+                    found = True
+                    total += new_h * 100 + new_v
+                    print(f"Found at {x = } and {y = }")
+                    break
+
+        if found:
+            break
+
+    print(total)
 
 
 if __name__ == "__main__":
