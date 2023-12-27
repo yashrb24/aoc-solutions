733af8c10e9d90b5d03112a1eae6329dad717f80
yashrb24 <bhisikaryash2403@gmail.com>
2023-12-27 10:43:05 +0530

    part 2 done

--- 2023/Day15/main.py
+++ 2023/Day15/main.py
@@ -1,20 +1,5 @@
 from collections import OrderedDict
 
-def part1(data):
-    strings = data[0].split(",")
-    total = 0
-    
-    for string in strings:
-        curr = 0
-        for c in string:
-            curr += ord(c)
-            curr *= 17
-            curr %= 256
-        total += curr
-
-    print(total)
-
-
 def custom_hash(label):
     curr = 0
     for c in label:
@@ -23,45 +8,45 @@ def custom_hash(label):
         curr %= 256
     return curr
 
+
+def part1(data):
+    strings = data[0].split(",")
+    total = sum(custom_hash(string) for string in strings)
+
+    print(total)
+
 def part2(data):
-    boxes = {i: [] for i in range(5)}
-    boxes_available = {i: set() for i in range(5)}
-    boxes_label_last = {i : dict() for i in range(5)}
+    boxes = {i: OrderedDict() for i in range(256)}
+    boxes_available = {i: set() for i in range(256)}
 
     strings = data[0].split(",")
-    
+
     for string in strings:
+
         if "-" in string:
             label = string[:-1]
             box_value = custom_hash(label)
             if label in boxes_available[box_value]:
                 boxes_available[box_value].remove(label)
-                boxes_label_last[box_value].pop(label)
-
+                boxes[box_value].pop(label)
+        
         else:
             label, focal_length = string.split("=")
             box_value = custom_hash(label)
 
-            if label not in boxes_available[box_value]:
-                boxes_available[box_value].add(label)
+            boxes_available[box_value].add(label)
+            boxes[box_value][label] = focal_length
 
-            boxes_label_last[box_value][label] = focal_length
-            boxes[box_value].append((label, focal_length))
+    total = 0
+    for box_idx, box in boxes.items():
+        for i, focal_length in enumerate(box.values(), start=1):
+            total += int(focal_length) * i * (box_idx + 1)
 
-    # prune the entries not required
-    final_boxes = {i: [] for i in range(5)}
-    for box_idx, strings in boxes.items():
-        for label, focal_length in strings:
-            if label in boxes_available[box_idx] and focal_length == boxes_label_last[box_idx][label]:
-                final_boxes[box_idx].append((label, focal_length))
-
-    print(final_boxes)
+    print(total)
 
 
-
-
-if __name__ == '__main__':
-    with open('input.txt') as f:
+if __name__ == "__main__":
+    with open("input.txt") as f:
         data = f.read().splitlines()
     part1(data)
     part2(data)
