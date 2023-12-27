07a5cd2163e9d03d3633a1316ba068e557aca901
yashrb24 <bhisikaryash2403@gmail.com>
2023-12-27 10:23:41 +0530

    part 2 progress

--- 2023/Day15/main.py
+++ 2023/Day15/main.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 def part1(data):
     strings = data[0].split(",")
     total = 0
@@ -13,8 +15,49 @@ def part1(data):
     print(total)
 
 
+def custom_hash(label):
+    curr = 0
+    for c in label:
+        curr += ord(c)
+        curr *= 17
+        curr %= 256
+    return curr
+
 def part2(data):
-    pass
+    boxes = {i: [] for i in range(5)}
+    boxes_available = {i: set() for i in range(5)}
+    boxes_label_last = {i : dict() for i in range(5)}
+
+    strings = data[0].split(",")
+    
+    for string in strings:
+        if "-" in string:
+            label = string[:-1]
+            box_value = custom_hash(label)
+            if label in boxes_available[box_value]:
+                boxes_available[box_value].remove(label)
+                boxes_label_last[box_value].pop(label)
+
+        else:
+            label, focal_length = string.split("=")
+            box_value = custom_hash(label)
+
+            if label not in boxes_available[box_value]:
+                boxes_available[box_value].add(label)
+
+            boxes_label_last[box_value][label] = focal_length
+            boxes[box_value].append((label, focal_length))
+
+    # prune the entries not required
+    final_boxes = {i: [] for i in range(5)}
+    for box_idx, strings in boxes.items():
+        for label, focal_length in strings:
+            if label in boxes_available[box_idx] and focal_length == boxes_label_last[box_idx][label]:
+                final_boxes[box_idx].append((label, focal_length))
+
+    print(final_boxes)
+
+
 
 
 if __name__ == '__main__':
