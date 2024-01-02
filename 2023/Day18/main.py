4b3d507949ae4fdd70d686a8cbb2aab6d61845f2
yashrb24 <bhisikaryash2403@gmail.com>
2024-01-02 08:45:40 +0530

    part 2 done

--- 2023/Day18/main.py
+++ 2023/Day18/main.py
@@ -1,6 +1,4 @@
-import sys
-
-sys.setrecursionlimit(1000000)
+import numpy as np
 import queue
 
 
@@ -49,7 +47,42 @@ def part1(data):
 
 
 def part2(data):
-    pass
+    x_coords, y_coords = [], []
+    direction_dict = {"0": "R", "1": "U", "2": "L", "3": "D"}
+    movements = {"L": (0, -1), "R": (0, 1), "U": (-1, 0), "D": (1, 0)}
+
+    curr_x, curr_y = 0, 0
+    boundary_pts = 0
+
+    for line in data:
+        _, _, hex_code = line.split()
+        steps = int(hex_code[2:-2], 16)
+        direction = direction_dict[hex_code[-2]]
+
+        x_steps, y_steps = movements[direction]
+
+        curr_x = curr_x + x_steps * steps
+        curr_y = curr_y + y_steps * steps
+        boundary_pts += steps
+
+        x_coords.append(curr_x)
+        y_coords.append(curr_y)
+
+    x_coords = np.array(x_coords)
+    y_coords = np.array(y_coords)
+
+    i = np.arange(len(x_coords))
+    
+    # shoelace formula
+    area = np.abs(np.sum(x_coords[i-1] * y_coords[i] - x_coords[i] * y_coords[i-1]) * 0.5)
+
+    # interior points
+    interior_pts = area - boundary_pts * 0.5 + 1
+
+    answer = boundary_pts + interior_pts
+
+    print(int(answer))
+
 
 
 if __name__ == "__main__":
